9ad2371d32d9f4989af772b3bba4583ef7835491
Irina Kudriavtseva <st_irina_igorevna@mail.ru>
2023-10-21 00:09:45 +0500

    Add task4

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите три числа ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > number1) max = number2;
+if (number3 > max) max = number3;
+Console.WriteLine(max);
+
