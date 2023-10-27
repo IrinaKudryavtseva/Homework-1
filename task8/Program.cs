9af78667e3daaf216e085890497baf795f6b9b3a
Irina Kudriavtseva <st_irina_igorevna@mail.ru>
2023-10-27 23:04:18 +0500

    add task8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+int number = new Random().Next(1,20);
+Console.WriteLine("N = " + number);
+for (int N = 1; N <= number; N++)
+if (N % 2 == 0)
+{
+Console.Write(N + " ");
+}
+
